0ce6802647e82ea1d74cc123b422b7023ace1fd1
Jenkinsss-05 <144263297+Jenkinsss-05@users.noreply.github.com>
2023-10-07 19:20:00 -0600

    Add files via upload

---
+++ Tarea2 Diseño web/js/reto8.js
@@ -0,0 +1,7 @@
+document.getElementById('instagram-link').addEventListener('click', function() {
+    alert('Síguenos en instagram');
+});
+
+document.getElementById('facebpoñ-link').addEventListener('click', function() {
+    alert('Síguenos en facebook');
+});
